7375390be97f092f3033ac32397d4fb9970f4dfd
cc-aeya <aaminah.x@icloud.com>
2022-12-12 11:45:01 +0000

    appended 1st row separetly, adjusted .format(), css not working

--- assests/js/script.js
+++ assests/js/script.js
@@ -15,18 +15,21 @@
 
 //GLOBALS
 var mainEl = $('.container');
-var startWorkDay = moment(09, 'HHA');
+var startWorkDay = moment(9, 'hA');
 // console.log(startWorkDay);
 
 //CURRENT DAY
 var currentD = moment().format('MMMM Do YYYY');
 $('#currentDay').text(currentD);
 
+//APPENDING FIRST ROW
+mainEl.append(`<div class='row time-block'>`+`${startWorkDay.format('hh A')}`+`</div>`)
+
 //TIME-BLOCK LOOP
-    while (startWorkDay.hour() < 18) {
+    while (startWorkDay.hour() < 17) {
         startWorkDay.add(1, 'hours')
         mainEl.append(
-            `<div class=row time-block>`+`${startWorkDay.format('HH A')}`+`</div>`)
+            `<div class=row time-block>`+`${startWorkDay.format('h A')}`+`</div>`)
     }
     
 
