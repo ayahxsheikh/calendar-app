312be00f9c6302d7f12a5938197e9b902bc85778
cc-aeya <aaminah.x@icloud.com>
2022-12-14 00:31:52 +0000

    checking current time sec console

--- assests/js/script.js
+++ assests/js/script.js
@@ -28,38 +28,25 @@ while (startWorkDay.hour() < 18) {
     startWorkDay.add(1, 'hours');
 }
 
-//CHECKING CURRENT TIME:
 
-// var now = moement().format('h A');
-// var hour = startWorkDay.format('h A');
-
-//     if (hour < now){
-//         textarea.addClass('.past');
-
-//     } else if (hour > now){
-    //         textarea.addClass('.future')
-    
-    //     } else hour = textarea.addClass('.present')
-    
-    
 //BUTTON
-    var button = $('.saveBtn');
-    button.on('click', storeItem);
-    
-    
-    // SAVE TO LOCAL STORAGE
-    function storeItem(){
-        //created vars to reference the value of textarea and the id of startWorkDay hour -- targetting them using $(this) 
-        var text = $(this).siblings('textarea').val();
-        console.log(text)
-    var hour = $(this).parent().attr('id');
+var button = $('.saveBtn');
+button.on('click', storeItem);
 
+
+// SAVE TO LOCAL STORAGE
+function storeItem(){
+    //created vars to reference the value of textarea and the id of startWorkDay hour -- targetting them using $(this) 
+    var text = $(this).siblings('textarea').val();
+    console.log(text)
+    var hour = $(this).parent().attr('id');
+    
     //text and idHour are vars: no need for strings
     localStorage.setItem(hour, text);
     displayMessage();
 }
 
-// DISPLAY text saved to localStorage:
+// DISPLAY text saved to localStorage: make into loop
 $('#hour9').children('textarea').val(localStorage.getItem('hour9'));
 $('#hour10').children('textarea').val(localStorage.getItem('hour10'));
 $('#hour11').children('textarea').val(localStorage.getItem('hour11'));
@@ -71,6 +58,20 @@ $('#hour4').children('textarea').val(localStorage.getItem('hour4'));
 $('#hour5').children('textarea').val(localStorage.getItem('hour5'));
 
 
+//CHECKING CURRENT TIME:
+
+var now = moment().format('h A');
+var hour = startWorkDay.hour();
+console.log(hour);
+console.log(now);
+
+    if (hour < now){
+        $('textarea').addClass('.past');
+
+    } else if (hour > now){
+            $('textarea').addClass('.future')
+    
+        } else hour = $('textarea').addClass('.present')
 
 
 // SET TIMEOUT
